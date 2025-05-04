f3d536de4ccc3e9c515dec88c51d726926faae94
Vendula Zbořilová <vendulazborilova@gmail.com>
2025-05-04 15:27:09 +0200

    Oživení všech políček, střídání tahů - křížek, kroužek, zjištění výherce, restart hry a oznámení výherce

--- hra.js
+++ hra.js
@@ -1,32 +1,89 @@
-let currentPlayer = 'circle'; 
+// načte se funkce findWinner z externí knihovny
+// tím se zjistí, jestli někdo vyhrál
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 
-const circleIcon = document.querySelector(".circle__icon"); 
-const allButtons = document.querySelectorAll(".square"); 
+// nastavím, že jako první hraje kolečko
+let currentPlayer = 'circle';
+
+// najde se HTML element obrázku vedle textu HRAJE:
+
+const playerSymbol = document.querySelector('.symbol');
+
+// funkce, která vrátí pole se stavem celé hry
+// obsahuje těch 100 položek: 'x', 'o' nebo '_' podle políček
+const getBoard = () => {
+  const fields = document.querySelectorAll('.square');
+  const board = [];
+
+  fields.forEach((field) => {
+    if (field.classList.contains('board__field--cross')) {
+      board.push('x');
+    } else if (field.classList.contains('board__field--circle')) {
+      board.push('o');
+    } else {
+      board.push('_');
+    }
+  });
+
+  return board;
+};
+
+// funkce, která se spustí při kliknutí na jedno políčko
 
 const handleFieldClick = (event) => {
   const clickedField = event.target;
-  
-  clickedField.disabled = true; 
 
-  if (currentPlayer === "circle") {
-    clickedField.classList.add("board__field--circle");
-    circleIcon.innerHTML = `<img src="cross.svg" alt="křížek" class="cross">`;
-    currentPlayer = "cross";
-  } else if (currentPlayer === "cross") {
-    clickedField.classList.add("board__field--cross");
-    circleIcon.innerHTML = `<img src="circle.svg" alt="kroužek" class="circle">`;
-    currentPlayer = "circle";
+   // pokud už je políčko obsazené = nelze do něj hrát
+  if (
+    clickedField.classList.contains('board__field--circle') ||
+    clickedField.classList.contains('board__field--cross')
+  ) {
+    return;
+  }
+
+   //  podle toho, kdo je na tahu = změní se ikonka
+  if (currentPlayer === 'circle') {
+    clickedField.classList.add('board__field--circle');
+    playerSymbol.src = 'cross.svg';
+    currentPlayer = 'cross';
+  } else {
+    clickedField.classList.add('board__field--cross');
+    playerSymbol.src = 'circle.svg';
+    currentPlayer = 'circle';
+  }
+
+   // žádné další klikání na stejné políčko
+  clickedField.disabled = true;
+
+
+  // zjistím stav celé hry (100 políček) => jestli někdo vyhrál
+  const board = getBoard();
+  const winner = findWinner(board);
+
+  console.log(board);
+  console.log(board.length);
+  console.log('Vítěz:', winner);
+
+
+  // někdo vyhrál => ukáže se hláška a načte se hra znovu
+  if (winner === 'x' || winner === 'o') {
+    setTimeout(() => {
+      alert(`Vyhrál hráč se symbolem ${winner === 'x' ? 'křížek' : 'kolečko'}!`);
+      location.reload();
+    }, 100);
   }
 };
 
-allButtons.forEach((button) => {
-  button.addEventListener("click", handleFieldClick);
+// připojit posluchače až po správné funkce
+const allFields = document.querySelectorAll('.square');
+allFields.forEach((field) => {
+  field.addEventListener('click', handleFieldClick);
 });
 
-const restartButton = document.querySelector(".icon-button.blue");
-
-restartButton.addEventListener("click", (event) => {
-  const reset = confirm("Opravdu chcete hru restartovat?");
+// dotaz na potvrzení RESTART
+const restartButton = document.querySelector('.icon-button.blue');
+restartButton.addEventListener('click', (event) => {
+  const reset = confirm('Opravdu chcete hru restartovat?');
   if (!reset) {
     event.preventDefault();
   }
